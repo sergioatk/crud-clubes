161648762cd9b942f9c3cb0eecc707daab898e45
sergioatk <55900410+sergioatk@users.noreply.github.com>
2021-08-12 11:47:02 -0300

    actualiza la logica del servidor

--- template-engine/index.js
+++ template-engine/index.js
@@ -9,9 +9,7 @@ const app = express();
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create();
 
-//const static = path.join(__dirname, '/uploads/imagenes')
-
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, '/uploads/images')));
 
 let storage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -24,35 +22,20 @@ let storage = multer.diskStorage({
 })
 
 const upload = multer({ storage });
-
-
-
-
 const { send } = require('process');
-
-
-
-
-const equipos = [];
-
-
+let equipos = [];
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: true }));
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-
-
 app.get('/', (req, res) => {
-
-
     res.render('home', {
         layout: 'boiler',
         data: {
             titulo: 'Lista de Clubes de Inglaterra',
-            subTitulo: 'Podes agregar o eliminar equipos',
+            subTitulo: 'Lista actual',
             equipos
         }
     })
@@ -70,22 +53,78 @@ app.get('/equipos', (req, res) => {
     
 });
 
-function asignarId(arr) {
 
+app.post('/api/equipos', upload.single('imagen'), (req, res) => {
+    
+    const urlImg = req.file.filename;
+    const { nombre, pais } = req.body;
+    const nuevoEquipo = new Equipo(nombre, pais, urlImg);
+    equipos.push(nuevoEquipo);
+    res.send('Equipo cargado correctamente');
+})
+
+
+app.get('/api/equipos/:nombreEquipo', (req, res) => {
+    
+    const { nombreEquipo } = req.params;
+
+    const index = equipos.findIndex(e => e.nombre === nombreEquipo);
+  
+
+    if (index < 0) {
+        res.status(404).send(`no encontramos a ${nombreEquipo} en nuestra base de datos`)
+        return;
+    } else {
+        res.status(200).json(equipos[index]);
+    }
+});
+
+app.get('/api/lista-equipos', (req, res) => {
+    if (!equipos[0]) {
+        res.status(404).send(`No existen equipos para enviar, crealos desde la seccion de equipos!`);
+        return;
+    }
+    res.status(200).json(equipos);
+})
+
+app.put('/api/equipos/:nombreEquipo', (req, res) => {
+
+    const { nombreEquipo } = req.params;
+    const index = equipos.findIndex(e => e.nombre === nombreEquipo);
+    const nuevoNombre = req.body.nombre;
+    const nuevoPais = req.body.pais;
+
+    console.log(`entramos a put`)
+
+    if (index < 0) {
+        res.status(404).send(`no encontramos a ${nombreEquipo} en nuestra base de datos, intente con un equipo registrado`);
+        return;
+    }
+
+    equipos[index].nombre = nuevoNombre;
+    equipos[index].pais = nuevoPais;
+    res.status(202).send(`el equipo ${nombreEquipo}, paso a llamarse ${nuevoNombre} correctamente, gracias por usar nuestro servicio.`)
+    
+})
+
+function removerUnItemDeArray(array, indexARemover) {
+    array.splice(indexARemover, 1)
 }
 
-app.post('/equipos', upload.single('imagen'), (req, res) => {
+app.delete('/borrarequipo/:equipoABorrar', (req, res) => {
+    const { equipoABorrar } = req.params;
+    const index = equipos.findIndex(e => e.nombre === equipoABorrar);
+
+    if (index < 0) {
+        res.status(404).send(`El equipo ${equipoABorrar}, no se encuentra en nuestra base de datos`);
+        return;
+    }
+
+    res.status(200);
+    removerUnItemDeArray(equipos, index)
+    res.send(`Se elimino ${equipoABorrar} correctamente`)
+
     
-    const { nombre, pais } = req.body
-    const urlImg = path.join(__dirname, req.file.filename);
-
-    const nuevoEquipo = new Equipo(nombre, pais, urlImg);
-
-
-    equipos.push(nuevoEquipo);
-
- 
-    res.send(`Cargaste correctamente el siguiente equipo: Nombre ${nombre}, Pais: ${pais}`)
 })
 
 app.listen(PUERTO);
